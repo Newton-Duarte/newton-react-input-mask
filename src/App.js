ead83fc814ba986532f341e51ba9e78cf8fd8eb4
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-04-08 09:28:53 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./styles.css";
 
 export default function App() {
   const [cpf, setCpf] = useState("");
+  const [cnpj, setCnpj] = useState("");
 
   return (
     <div className="App">
@@ -13,6 +14,13 @@ export default function App() {
         onChange={(event) => setCpf(event.target.value)}
       />
       <pre>{cpf}</pre>
+
+      <MaskedInput
+        mask="cnpj"
+        value={cnpj}
+        onChange={(event) => setCnpj(event.target.value)}
+      />
+      <pre>{cnpj}</pre>
     </div>
   );
 }
--- src/MaskedInput.js
+++ src/MaskedInput.js
@@ -2,18 +2,27 @@ import InputMask from "react-input-mask";
 
 const convertToNumbers = (value) => value.replace(/[^0-9]/g, "");
 
-export function MaskedInput({ value, onChange }) {
+const CPF_MASK = "999.999.999-99";
+const CNPJ_MASK = "99.999.999/9999-99";
+
+const getInputMask = (mask = "cpf") => (mask === "cpf" ? CPF_MASK : CNPJ_MASK);
+
+export function MaskedInput({ value, onChange, mask = "cpf" }) {
   const handleChange = (event) => {
     onChange({
       ...event,
       target: {
         ...event.target,
-        value: convertToNumbers(event.target.value)
-      }
+        value: convertToNumbers(event.target.value),
+      },
     });
   };
 
   return (
-    <InputMask mask="999.999.999-99" value={value} onChange={handleChange} />
+    <InputMask
+      mask={getInputMask(mask)}
+      value={value}
+      onChange={handleChange}
+    />
   );
 }
